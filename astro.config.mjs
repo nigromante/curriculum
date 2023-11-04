2166cfb1b6329e02529c36fd4a9ce1e08998c2ab
“nigromante” <julianvidal@live.cl>
2023-11-04 15:35:16 -0300

    gh pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://nigromante.github.io',
-  base: '/nigromante.github.io',
+  base: '/curriculum',
 });
